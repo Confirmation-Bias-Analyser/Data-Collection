cf4ace12a506e9f06e649b40fa5149781a21456f
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2022-02-06 23:21:15 +0800

    Made adjustments to get URLs from comments

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 import psycopg2
+import re
+import requests
+import urllib
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 import pandas as pd
 
 def setUpDB(command, url):
@@ -35,3 +40,82 @@ def getData(command, url):
         
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+
+# def getLinks(string):
+#     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+#     allLinks = re.findall(regex, string)
+    
+#     if len(allLinks) > 1:
+#         result = ''
+        
+#         for i in allLinks:
+#             result += i[0] + ';'
+        
+#         return result
+    
+#     elif len(allLinks) == 1: return allLinks[0]
+#     else: return ''
+
+def getLinks(string):
+    urls = re.findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", string)
+    links = ''
+    
+    for url in urls:
+        try:
+            opener = urllib.request.build_opener()
+            request = urllib.request.Request(url)
+            response = opener.open(request)
+            actual_url = response.geturl()
+                        
+            if '](' in actual_url:
+                actual_url = actual_url.split('](')[0]
+            
+            links += actual_url + ';'
+            
+            
+        except:
+            if '](' in url:
+                url = url.split('](')[0]
+            
+            links += url + ';'
+
+    return links
+    
+# def getLinkTitle(url):
+#     reqs = requests.get(url)
+#     soup = BeautifulSoup(reqs.text, 'html.parser')
+#     result = ''
+
+#     for title in soup.find_all('title'):
+#         result += title.get_text()
+
+#     return result
+    
+def getURLfromList(url):
+    if ';' in url:
+        url = url.split(';')[:-1]
+        result = []
+        
+        for i in url:
+            result.append(urlparse(i).hostname)
+            
+        return result
+    
+    else:
+        return ''
+
+# def getURLfromList(url):
+#     if len(url) > 0:
+#         result = []
+        
+#         for i in url:
+#             try:
+#                 result.append(getLinkTitle(i))
+                
+#             except: continue
+            
+#         if len(result) > 0: return result
+#         else: return ''
+    
+#     else:
+#         return ''
